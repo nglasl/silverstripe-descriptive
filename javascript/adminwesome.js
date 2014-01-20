3c823a6e1f44f37deb8ed56397a19f410bc4152a
Nathan Glasl <nathan@silverstripe.com.au>
2014-01-20 16:38:17 +1100

    Fixed the display bug when scrolling across the menu quickly.

--- javascript/adminwesome.js
+++ javascript/adminwesome.js
@@ -6,13 +6,15 @@
 		$('body').append("<div id='cms-menu-captionwesome'></div>");
 		var element = $('div#cms-menu-captionwesome');
 		element.toggleClass('caption');
+		var show;
 
 		$.entwine('ss', function($) {
 
 			// Display the caption for the menu.
 
-			$('div#cms-menu ul.cms-menu-list li:not(.children)').entwine({
+			$('div#cms-menu ul.cms-menu-list li').entwine({
 				onmouseenter: function() {
+					clearTimeout(show);
 					element.hide();
 					var caption = $(this).find('div.captionawesome-description').first();
 					if(caption.length) {
@@ -20,7 +22,9 @@
 						var left = $(this).width();
 						element.css({top: top, left: left, right: 'auto'});
 						element.html(caption.html());
-						element.stop(true).fadeIn();
+						show = setTimeout(function() {
+							element.fadeIn(250);
+						}, 250);
 					}
 				}
 			});
@@ -29,7 +33,8 @@
 
 			$('div#cms-menu ul.cms-menu-list').entwine({
 				onmouseleave: function() {
-					element.stop(true).fadeOut();
+					clearTimeout(show);
+					element.stop(true).fadeOut(250);
 				}
 			});
 		});
