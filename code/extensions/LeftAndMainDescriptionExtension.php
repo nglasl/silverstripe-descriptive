f105a8e3c7ce7b36b1f36e9130394448e1b65db5
Nathan Glasl <nathan@silverstripe.com.au>
2014-01-21 10:28:25 +1100

    Cleaning up class definitions.

--- code/extensions/LeftAndMainDescriptionExtension.php
+++ code/extensions/LeftAndMainDescriptionExtension.php
@@ -1,25 +1,30 @@
 <?php
 
 /**
- *	Adminwesome extension which allows model admin descriptions to be pulled in from the menu template.
+ *	Adminwesome extension which will allow model admin descriptions to be pulled into the CMS template.
  *	@author Nathan Glasl <nathan@silverstripe.com.au>
  */
 
 class LeftAndMainDescriptionExtension extends Extension {
 
 	/**
-	 *	Add a model admin description to each CMS menu tab.
+	 *	Add the model admin description against each CMS menu tab.
 	 */
 
 	public function updatedMainMenu() {
 
 		$tabs = $this->owner->MainMenu();
 		foreach($tabs as $tab) {
-			if(class_exists($tab->MenuItem->controller) && is_subclass_of($tab->MenuItem->controller, 'ModelAdmin')) {
-				$tab->setField('Description', Config::inst()->get($tab->MenuItem->controller, 'menu_description'));
+			$class = $tab->MenuItem->controller;
+			if(class_exists($class) && is_subclass_of($class, 'ModelAdmin')) {
+				$tab->setField('Description', Config::inst()->get($class, 'menu_description'));
 			}
 		}
+
+		// Allow extension customisation.
+
 		$this->owner->extend('updateMainMenu', $tabs);
 		return $tabs;
 	}
+
 }
