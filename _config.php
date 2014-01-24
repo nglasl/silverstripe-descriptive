1fe57c2f88b91cfd2385c35fe03d51f338069bc4
Nathan Glasl <nathan@silverstripe.com.au>
2014-01-24 15:45:11 +1100

    Cleaned up the service definition.

--- _config.php
+++ _config.php
@@ -8,6 +8,4 @@
 if(!defined('ADMINWESOME_PATH')) {
 	define('ADMINWESOME_PATH', rtrim(basename(dirname(__FILE__))));
 }
-Object::add_extension('LeftAndMain', 'LeftAndMainDescriptionExtension');
-LeftAndMain::require_javascript(ADMINWESOME_PATH . '/javascript/adminwesome.js');
-LeftAndMain::require_css(ADMINWESOME_PATH . '/css/adminwesome.css');
+AdminwesomeService::apply_required_extensions();
--- code/services/AdminwesomeService.php
+++ code/services/AdminwesomeService.php
@@ -1,19 +1,52 @@
 <?php
 
+/**
+ *	Handles any user customisation.
+ *	@author Nathan Glasl <nathan@silverstripe.com.au>
+ */
+
 class AdminwesomeService {
+
+	/**
+	 *	Apply all Adminwesome required extensions.
+	 */
+
+	public static function apply_required_extensions() {
+
+		Object::add_extension('LeftAndMain', 'LeftAndMainDescriptionExtension');
+		LeftAndMain::require_javascript(ADMINWESOME_PATH . '/javascript/adminwesome.js');
+		LeftAndMain::require_css(ADMINWESOME_PATH . '/css/adminwesome.css');
+	}
+
+	/**
+	 *	Apply custom styles to the absolute description element.
+	 *
+	 *	@parameter <{DESCRIPTION_CSS}> array(string)
+	 */
 
 	public static function customise_css($styles) {
 
 		if(is_array($styles)) {
-			$path = ADMINWESOME_PATH . '/css/custom.css';
-			$file = Director::baseFolder() . '/' . $path;
-			$css = '#cms-menu-adminwesome {' . PHP_EOL;
+
+			// Determine the path to write against.
+
+			$URL = ADMINWESOME_PATH . '/css/custom.css';
+			$path = Director::baseFolder() . "/{$URL}";
+
+			// Collate each custom style where appropriate.
+
+			$CSS = '#cms-menu-adminwesome {' . PHP_EOL;
 			foreach($styles as $style => $value) {
-				$css .= "\t{$style}: {$value} !important;" . PHP_EOL;
+				if(!is_numeric($style)) {
+					$CSS .= "\t{$style}: {$value} !important;" . PHP_EOL;
+				}
 			}
-			$css .= '}' . PHP_EOL;
-			file_put_contents($file, $css);
-			LeftAndMain::require_css($path);
+			$CSS .= '}' . PHP_EOL;
+
+			// Write the custom styles.
+
+			file_put_contents($path, $CSS);
+			LeftAndMain::require_css($URL);
 		}
 	}
 
